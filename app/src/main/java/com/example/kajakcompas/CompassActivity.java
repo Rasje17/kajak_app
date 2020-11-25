e4f378735aa4444a140dc16ceb4276f825af22e3
Rasmus Jensen <rasje17@student.sdu.dk>
2020-11-25 20:44:10 +0100

    added direction arrow with dummy functionality
    do doo, implementing gps logging and handling of gps bearing and distance and going from point to point

--- app/src/main/java/com/example/kajakcompas/CompassActivity.java
+++ app/src/main/java/com/example/kajakcompas/CompassActivity.java
@@ -2,26 +2,123 @@ package com.example.kajakcompas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-public class CompassActivity extends AppCompatActivity {
-    ImageView compasrose;
+public class CompassActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private final float[] accelerometerReading = new float[3];
+    private final float[] magnetometerReading = new float[3];
 
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
+
+    private long timestamp = 0;
+    ImageView compasrose;
+    ImageView direction;
+
+    float fixed_test_direction = (float) 90;
+    int testcount = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
 
+        //getting the compas image vev on the activity
         compasrose = findViewById(R.id.compas_iw);
-        compasrose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                compasrose.setRotation(90);
-            }
-        });
+        direction = findViewById(R.id.direction);
 
+        //getting access to sensor manager
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+        // You must implement this callback in your code
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Get updates from the accelerometer and magnetometer at a constant rate.
+        // To make batch operations more efficient and reduce power consumption,
+        // provide support for delaying updates to the application.
+        //
+        // In this example, the sensor reporting delay is small enough such that
+        // the application receives an update before the system checks the sensor
+        // readings again.
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (magneticField != null) {
+            sensorManager.registerListener(this, magneticField,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Don't receive any more updates from either sensor.
+        sensorManager.unregisterListener(this);
+    }
+
+    // Get readings from accelerometer and magnetometer. To simplify calculations,
+    // consider storing these readings as unit vectors.
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading,
+                    0, accelerometerReading.length);
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading,
+                    0, magnetometerReading.length);
+        }
+        // use timestamp verifacatin to reduce amount of calculations
+        long check_time = System.currentTimeMillis();
+        if (timestamp < check_time -1000) {
+            updateOrientationAngles();
+            timestamp = check_time;
+        }
+    }
+
+    // Compute the three orientation angles based on the most recent readings from
+    // the device's accelerometer and magnetometer.
+    public void updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(rotationMatrix, null,
+                accelerometerReading, magnetometerReading);
+
+        // "rotationMatrix" now has up-to-date information.
+
+        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+
+        // "orientationAngles" now has up-to-date information.
+
+        compasrose.setRotation(convertToDegrees(orientationAngles[0]));
+        Log.d("azimut", String.valueOf(convertToDegrees(orientationAngles[0])));
+
+        direction.setRotation(convertToDegrees(orientationAngles[0]) + testcount);
+
+        testcount+= 5;
+
+    }
+    private float convertToDegrees(float radiansAngle){
+        float degreeangle = (float) (radiansAngle*(180/Math.PI));
+        return degreeangle;
     }
 
 }
