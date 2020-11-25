8c24d42a97782f802c7e115c70e0178c1d870bca
Rasmus Jensen <rasje17@student.sdu.dk>
2020-11-25 10:25:14 +0100

    kompas angle test
    successfully turned the compas

--- app/src/main/java/com/example/kajakcompas/CompassActivity.java
+++ app/src/main/java/com/example/kajakcompas/CompassActivity.java
@@ -3,12 +3,25 @@ package com.example.kajakcompas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CompassActivity extends AppCompatActivity {
+    ImageView compasrose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+
+        compasrose = findViewById(R.id.compas_iw);
+        compasrose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compasrose.setRotation(90);
+            }
+        });
+
     }
+
 }
