2cf57113189428f13fd489ef8cabeb851e8ccb6c
Rasmus Jensen <rasje17@student.sdu.dk>
2020-11-27 10:35:48 +0100

    init location handling
    missing permission handling implementation and implementation of handling of route information

--- app/src/main/java/com/example/kajakcompas/CompassActivity.java
+++ app/src/main/java/com/example/kajakcompas/CompassActivity.java
@@ -1,19 +1,26 @@
 package com.example.kajakcompas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-public class CompassActivity extends AppCompatActivity implements SensorEventListener {
+public class CompassActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
     private SensorManager sensorManager;
+    private LocationManager locationManager;
     private final float[] accelerometerReading = new float[3];
     private final float[] magnetometerReading = new float[3];
 
@@ -23,9 +30,12 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     private long timestamp = 0;
     ImageView compasrose;
     ImageView direction;
+    long gpsMinTimeMS = 1000;
+    float gpsMinDistanceM = 10;
 
-    float fixed_test_direction = (float) 90;
-    int testcount = 1;
+    float direction_angle = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +47,18 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
 
         //getting access to sensor manager
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, gpsMinTimeMS, gpsMinDistanceM, this);
     }
 
     @Override
@@ -105,20 +126,71 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         // "rotationMatrix" now has up-to-date information.
 
         SensorManager.getOrientation(rotationMatrix, orientationAngles);
-
         // "orientationAngles" now has up-to-date information.
 
+
+        update_compas_and_direction_orientation();
+
+
+
+
+    }
+
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        check_for_goal(location);
+        update_direction_angle(location);
+        update_finalgoal_distance(location);
+
+        update_compas_and_direction_orientation();
+
+    }
+
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+
+    private void update_compas_and_direction_orientation() {
+        // set the correct orientation of compas image
         compasrose.setRotation(convertToDegrees(orientationAngles[0]));
         Log.d("azimut", String.valueOf(convertToDegrees(orientationAngles[0])));
 
-        direction.setRotation(convertToDegrees(orientationAngles[0]) + testcount);
-
-        testcount+= 5;
-
+        //change the direction arrow to allign with new compas alignment
+        direction.setRotation(convertToDegrees(orientationAngles[0]) + direction_angle);
     }
+
+    private void update_finalgoal_distance(Location location) {
+        //sum distance between curent location and next route lokation + nrext rout0 til route-1... to goal
+    }
+
+    private void update_direction_angle(Location location) {
+        //calculate angle from curent location to next route location
+    }
+
+    private void check_for_goal(Location location) {
+        //calculate if curent location is within threshold of next rout location if so check if goal if not cange next rout location to routlocation -1
+    }
+
     private float convertToDegrees(float radiansAngle){
         float degreeangle = (float) (radiansAngle*(180/Math.PI));
         return degreeangle;
     }
-
 }
